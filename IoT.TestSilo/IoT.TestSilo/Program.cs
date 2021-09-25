6987359e4a048ba7edffb43008c899c8fce9dd5f
Yuriy Lyeshchenko <yuriylsh@live.com>
2021-09-25 15:00:21 -0500

    Created projects

---
+++ IoT.TestSilo/IoT.TestSilo/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace IoT.TestSilo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
